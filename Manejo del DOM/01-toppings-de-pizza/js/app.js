ad7359822759baba83256fe8aab6321f90c0f63c
Osvaldo Ocampo <valam.osvaldo@gmail.com>
2023-07-23 12:09:04 -0600

    Clase 10
    Continuamos con el DOM
    * Estilos en JS
    * Modificar texto
    * Atributos
    * Clases
    * Crear y eliminar elementos

--- Manejo del DOM/01-toppings-de-pizza/js/app.js
+++ Manejo del DOM/01-toppings-de-pizza/js/app.js
@@ -39,3 +39,95 @@ const toppingsNaranja = document.querySelectorAll(".topping.bg-orange");
 console.log(toppingsNaranja);
 console.log(toppingsNaranja.length);
 console.log(toppingsNaranja[0]);
+
+// **** ESTILOS
+const primerTopping = document.querySelector(".topping");
+console.log(primerTopping);
+console.log(primerTopping.style);
+
+primerTopping.style.backgroundColor = "blue";
+// primerTopping.style.backgroundColor = "#6dff00";
+primerTopping.style.textTransform = "uppercase";
+
+// **** TEXTO
+const listaDeToppings = document.getElementById("list-toppings");
+console.log(listaDeToppings);
+console.log("> innerText");
+console.log(listaDeToppings.innerText);
+console.log("> textContent");
+console.log(listaDeToppings.textContent);
+console.log("> innerHTML");
+console.log(listaDeToppings.innerHTML);
+
+// ? Modificar texto
+const myTitle = document.getElementById("title");
+console.log(myTitle);
+console.log(myTitle.innerText);
+
+// myTitle.innerText = "Mis Toppings favoritos";
+
+// ****** ATRIBUTO
+const link = document.getElementsByTagName("a");
+console.log(link);
+console.log(link[0]);
+console.log(link[0].getAttribute("id"));
+console.log(link[0].getAttribute("href"));
+
+// ? Remover atributo
+// link[0].removeAttribute("href");
+
+// ? Modificar atributo
+link[0].setAttribute("href", "https://www.youtube.com/");
+
+// ? Agregando un atributo
+link[0].setAttribute("title", "Prueba de nuevo atributo");
+
+// ***** CLASES
+const myFirstTopping = document.querySelector(".topping");
+console.log(myFirstTopping);
+console.log(myFirstTopping.classList);
+console.log(myFirstTopping.classList.value);
+console.log(myFirstTopping.className);
+
+// ? Agregar una clase
+myFirstTopping.classList.add("green-text");
+console.log(myFirstTopping);
+
+// ? Verificar si una clase existe
+console.log(myFirstTopping.classList.contains("green-text"));
+
+// if (myFirstTopping.classList.contains("green-text")) {
+//   console.log("La clase existe");
+// } else {
+//   console.log("La clase NO existe");
+// }
+
+// ? Eliminar una clase
+myFirstTopping.classList.remove("green-text");
+
+// ***** CREAR ELEMENTOS
+// ? Referencia donde se va crear
+const listOriginalToppings = document.getElementById("list-toppings");
+console.log(listOriginalToppings);
+
+// ? Crear nuevo elemento
+const newTopping = document.createElement("li");
+
+// ? Agregando estilos CSS
+newTopping.classList.add("topping", "bg-brown");
+
+// ? Agregar contenido - Texto
+newTopping.innerText = "Queso Extra";
+
+// ? Agregar el elemento
+// listOriginalToppings.append(newTopping);
+listOriginalToppings.appendChild(newTopping);
+// listOriginalToppings.append("texto de prueba");
+
+const newChild = document.createElement("span");
+newChild.innerText = "Soy un hijo";
+
+newTopping.append(newChild);
+
+// ? Remover elemento
+newChild.remove();
